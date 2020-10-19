33665b788a1bda6b91f4a4cfc5ed00f7ad5e78a4
lauphern <lau.p.hern@hotmail.com>
2020-10-20 00:25:14 +0200

    added if statement for on sale items

--- shop/models.py
+++ shop/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Item:
-  id : int
-  name : str
+  id: int
+  name: str
   image: str
   price: int
+  sale: bool
--- shop/views.py
+++ shop/views.py
@@ -9,46 +9,55 @@ def index(request):
   item1.name = "Proin vel ligula"
   item1.image = "item-01.jpg"
   item1.price = 15.00
+  item1.sale = True
   
   item2 = Item()
   item2.name = "Erat odio rhoncus"
   item2.image = "item-02.jpg"
   item2.price = 25.00
+  item2.sale = True
 
   item3 = Item()
   item3.name = "Integer vel turpis"
   item3.image = "item-03.jpg"
   item3.price = 35.00
+  item3.sale = False
 
   item4 = Item()
   item4.name = "Sed purus quam"
   item4.image = "item-04.jpg"
   item4.price = 45.00
+  item4.sale = True
 
   item5 = Item()
   item5.name = "Morbi aliquet"
   item5.image = "item-05.jpg"
   item5.price = 55.00
+  item5.sale = True
 
   item6 = Item()
   item6.name = "Urna ac diam"
   item6.image = "item-06.jpg"
   item6.price = 65.00
+  item6.sale = False
 
   item7 = Item()
   item7.name = "Proin eget imperdiet"
   item7.image = "item-04.jpg"
   item7.price = 75.00
+  item7.sale = True
 
   item8 = Item()
   item8.name = "Nullam risus nisl"
   item8.image = "item-05.jpg"
   item8.price = 85.00
+  item8.sale = False
 
   item9 = Item()
   item9.name = "Cras tempus"
   item9.image = "item-06.jpg"
   item9.price = 95.00
+  item9.sale = False
 
   items = [item1, item2, item3, item4, item5, item6, item7, item8, item9]
 
